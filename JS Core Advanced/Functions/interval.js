451eb94230e4dbfd37edf01610c9cae8c593107b
unknown <pasha.khvostyuk@gmail.com>
2025-11-14 18:09:26 +0300

    Create interval.js

---
+++ JS Core Advanced/Functions/interval.js
@@ -0,0 +1,14 @@
+function repeat(operation, interval) {
+  
+    setInterval(operation, interval);
+
+}
+  
+function sayHello() {
+
+    console.log("Hello, World!");
+
+}
+  
+repeat(sayHello, 3000);
+  
